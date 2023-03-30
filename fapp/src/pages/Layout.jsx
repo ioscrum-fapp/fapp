d0b0c2b68eb7b663d6a2b41c9fc2ab89ff172e5a
Karol Wilk <karol.t.wilk@gmail.com>
2023-03-30 18:52:27 +0200

    Change logo to conform to style

--- fapp/src/pages/Layout.jsx
+++ fapp/src/pages/Layout.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import "../styles/Layout.css";
-import Logo from "../assets/Icon.svg"
+import Logo from "../assets/Icon.svg";
 
 export default function Layout() {
   return (
     <>
       <nav>
-        
         <ul>
           <li>
-            <img src={Logo} alt="logo"></img>
+            <img src={Logo} alt="logo" />
           </li>
           <li>
             <Link to="/">Home</Link>
