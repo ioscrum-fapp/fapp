f3c36f8002a07f41bff73ee0efa795ec5d580bf4
Karol Wilk <karol.t.wilk@gmail.com>
2023-04-18 13:55:50 +0200

    Fixed expenses linter errors

--- fapp/src/backend/expenseLogic.jsx
+++ fapp/src/backend/expenseLogic.jsx
@@ -3,14 +3,14 @@ import { v4 as uuid } from "uuid";
 const expensesRoute = "/expenses/";
 const url = "http://localhost:3030/expenses/";
 
-export function CreateNewExpense(navigate, userId, value, date, tags) {
+export default function CreateNewExpense(navigate, userId, value, date, tags) {
   const newUuid = uuid();
   const expense = {
     id: newUuid,
     user_id: userId,
-    value: value,
-    date: date,
-    tags: tags,
+    value,
+    date,
+    tags,
   };
 
   fetch(url, {
