ba530cd9be4755bdab3c5ade3ab6a7f55c2251b9
Adam Ćwikła <adam6xaolaw@gmail.com>
2023-05-16 01:31:01 +0200

    added view and return button

--- fapp/src/pages/CreateAccount/CreateAccount.jsx
+++ fapp/src/pages/CreateAccount/CreateAccount.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import "./CreateAccount.css";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { CreateNewAccount } from "../../backend/accountsLogic";
-
+import "./CreateAccount.css";
 const userId = 1;
 
 export default function CreateAccount() {
@@ -18,25 +16,39 @@ export default function CreateAccount() {
 
   return (
     <div className="CreateAccount">
-      <h1> Add new account </h1>{" "}
-      <form onSubmit={handleSubmit}>
-        <label> Name: </label>{" "}
-        <input
-          type="text"
-          required
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />{" "}
-        <label> Balance: </label>{" "}
-        <input
-          required
-          type="number"
-          step="0.01"
-          value={balance}
-          onChange={(e) => setBalance(e.target.value)}
-        />{" "}
-        <button type="button"> Create </button>{" "}
-      </form>{" "}
+      <h1> Add new account </h1>
+      <div className="GoBackButton">
+        <Link to="/accounts">
+            <button type="button" className="Button">
+              Go back
+            </button>
+          </Link>
+      </div>
+      <form onSubmit={handleSubmit} className="AddingForm">
+        <div className="formControl">
+          <label> Name: </label>
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="formControl">
+          <label> Balance: </label>
+          <input
+            required
+            type="number"
+            step="0.01"
+            min="0"
+            value={balance}
+            onChange={(e) => setBalance(e.target.value)}
+          />
+        </div>
+        <div className="formControl">
+          <button type="submit" className="submitButton"> Create </button>
+        </div>
+      </form>
     </div>
   );
 }
