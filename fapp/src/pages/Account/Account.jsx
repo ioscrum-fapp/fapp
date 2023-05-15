74230e53a599a4133d9dc815f887c43a80b6b84f
Michal-Szewc <szewcmichal@student.agh.edu.pl>
2023-05-16 00:37:31 +0200

    added account details view

--- fapp/src/pages/Account/Account.jsx
+++ fapp/src/pages/Account/Account.jsx
@@ -1,40 +1,44 @@
 import React from "react";
 import "./Account.css";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import useFetch from "../../hooks/useFetch.jsx";
+import useFetch from "../../hooks/useFetch";
+import { DeleteAccount } from "../../backend/accountsLogic";
 
-const url = "http://localhost:3030/accounts/";
+const BACKEND_URL = "http://localhost:3030/accounts/";
 const currency = "$";
 
-const Account = () => {
+export default function Account() {
   const { id } = useParams();
-  const { json, isFinished, error } = useFetch(
-    "http://localhost:3030/accounts/" + id
-  );
+  const { json, isFinished, error } = useFetch(BACKEND_URL + id);
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    fetch(url + id, {
-      method: "DELETE",
-    }).then(() => {
-      navigate("/accounts");
-    });
+  const handleClick = async () => {
+    await DeleteAccount(id);
+    navigate("/accounts/");
   };
 
   return (
-    <div className="Account">
-      Account component
-      {error && <div>{error}</div>}
-      {!isFinished && <div>Downloading account...</div>}
-      {json && <h1>Account: {json.name}</h1>}
-      {<Link to={`/accounts/`}>Go back</Link>}
-      {json && (
-        <p>
-          Balance: {currency} {json.balance}
-        </p>
-      )}
-      {json && <button onClick={handleClick}>Remove</button>}
-    </div>
+    <>
+      <Link to="/accounts/">
+        <button type="button" className="Button">
+          Go back
+        </button>
+      </Link>
+      <div className="Account">
+        {error && <div>{error}</div>}
+        {!isFinished && <div>Downloading account...</div>}
+        {json && <h1>Account: {json.name}</h1>}
+        {json && (
+          <p>
+            Balance: {currency} {json.balance}
+          </p>
+        )}
+        {json && (
+          <button type="button" className="Button" onClick={handleClick}>
+            Remove
+          </button>
+        )}
+      </div>
+    </>
   );
-};
-export default Account;
+}
