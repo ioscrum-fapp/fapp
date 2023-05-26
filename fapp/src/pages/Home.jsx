79c729c1d164512b0ffd95f82ff08c32478e4aad
MatthewCga <mateusz.cga@gmail.com>
2023-05-26 08:38:06 +0200

    added logo, improved style

--- fapp/src/pages/Home.jsx
+++ fapp/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Logo from "../assets/Icon2.svg";
+import {Link} from "react-router-dom";
 
 export default function Home() {
-  return <h1>Welcome to FAPP!!</h1>;
+  return <div>
+    <h1>Welcome to</h1>
+    <img src={Logo} alt="logo"/>
+    <h2>Fapping saves you time and money!</h2>
+    <h3>Add some fapping expenses in the <Link to="/expenses">Expense</Link> section</h3>
+  </div>;
 }
